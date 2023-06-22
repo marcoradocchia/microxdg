df1b2fb418baba17423c6e0b2da6a08212a34ac2
Marco Radocchia <marco.radocchia@outlook.com>
2023-06-23 01:01:21 +0200

    Removed `main.rs`.

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use microxdg::*;
-
-fn main() -> Result<(), XdgError> {
-    dbg!(std::mem::size_of::<Xdg>());
-    dbg!(std::mem::size_of::<XdgApp>());
-    dbg!(std::mem::size_of::<XdgError>());
-
-    Ok(())
-}
